be12bb3dc5edf613471e83858ebfaabfa792a611
Vincent <vincethewalker@yahoo.co.uk>
2020-05-21 16:35:25 +0100

    Completed Views and Controller set up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,6 @@ class HomeController < ApplicationController
 
   	if user_signed_in? 
   		redirect_to quizzes_path
-  	else
-  		redirect_to new_user_session_path
   	end
   
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,  controllers: { registrations: "registrations" }
-  root 'home#home'
+  root 'home#index'
   
   resources :quizzes
   get '/quizzes/:id/leaderboard' => "quizzes#leaderboard"
